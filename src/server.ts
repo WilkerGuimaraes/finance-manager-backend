1ceb70bc168c391e3c8805e9fa567908ab7b9185
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-08-27 17:26:52 -0300

    chore: add CORS setup

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import fastify from "fastify";
+import fastifyCors from "@fastify/cors";
 
 import { getTransactions } from "./routes/get-transactions";
 import { createNewTransaction } from "./routes/create-new-transaction";
 import { deleteTransaction } from "./routes/delete-transaction";
 
 const app = fastify();
+
+app.register(fastifyCors, {
+  origin: "*",
+  methods: ["GET", "POST", "DELETE"],
+});
 
 app.register(getTransactions);
 app.register(createNewTransaction);
